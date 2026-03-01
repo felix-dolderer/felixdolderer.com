931570f4ca4d6cb621a935c09b0917b3e023160f
Felix Dolderer <mail@felixdolderer.com>
2026-03-01 17:10:55 +0100

    copy some stuff from template

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <UContainer class="sm:border-x border-default pt-10">
+      <slot />
+      <AppNavigation :links="navLinks" />
+      <AppFooter />
+    </UContainer>
+  </div>
+</template>
